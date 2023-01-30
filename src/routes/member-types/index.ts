23e693fcb7b29146bde2a4df296db0b9f91d436d
redcliphaloe <redcliphaloe@gmail.com>
2023-01-30 19:09:51 +0300

    feat: modify routes\member-types

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -2,13 +2,14 @@ import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-sc
 import { idParamSchema } from '../../utils/reusedSchemas';
 import { changeMemberTypeBodySchema } from './schema';
 import type { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
+import ErrorMsg from '../types';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<
-    MemberTypeEntity[]
-  > {});
+  fastify.get('/', async function (request, reply): Promise<MemberTypeEntity[]> {
+    return await fastify.db.memberTypes.findMany();
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +18,13 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      const memberType = await fastify.db.memberTypes.findOne({key: 'id', equals: request.params.id});
+      if (memberType) {
+        return memberType;
+      }
+      throw new Error(ErrorMsg.member_type_not_found);
+    }
   );
 
   fastify.patch(
@@ -28,7 +35,9 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      return await fastify.db.memberTypes.change(request.params.id, request.body);
+    }
   );
 };
 
--- src/routes/types.d.ts
+++ src/routes/types.d.ts
@@ -1,7 +1,8 @@
 enum ErrorMsg {
   user_not_found = 'User not found',
   profile_not_found = 'Profile not found',
-  post_not_found = 'Post not found'
+  post_not_found = 'Post not found',
+  member_type_not_found = 'Member type not found'
 };
 
 export default ErrorMsg;
