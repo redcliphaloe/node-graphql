23ae6c9b4ca9a8ce6dfd540f822bd8ab2264b3d0
redcliphaloe <redcliphaloe@gmail.com>
2023-01-30 22:52:53 +0300

    refactor: remove routes\types

--- src/routes/types.d.ts
+++
@@ -1,8 +0,0 @@
-enum ErrorMsg {
-  user_not_found = 'User not found',
-  profile_not_found = 'Profile not found',
-  post_not_found = 'Post not found',
-  member_type_not_found = 'Member type not found'
-};
-
-export default ErrorMsg;
